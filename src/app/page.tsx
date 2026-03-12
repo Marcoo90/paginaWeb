6584c0fe5742c77b20000f2330a723a0ec8a7e62
unknown <marcomasg4@gmail,com>
2026-03-12 09:02:07 -0500

    actualizacion de salto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,8 +32,8 @@ export default function Home() {
 
               <SlideUp delay={0.3} duration={0.8}>
                 <h1 className="text-4xl md:text-6xl font-bold tracking-tight mb-6 leading-tight drop-shadow-lg">
-                  KORY MAQUI <br className="hidden md:block" />
-                  <span className="text-amber-500">GROUP MOLLO</span>
+                  KORY MAQUI {/*<br className="hidden md:block" />*/}
+                  <span className="text-amber-500"><br />GROUP MOLLO</span>
                 </h1>
               </SlideUp>
 
